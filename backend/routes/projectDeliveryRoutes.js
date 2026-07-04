11f2b57cdb307a13b19110e258df30b5b49c65f7
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-04 14:15:31 +0530

    Add authentication to project delivery routes

--- backend/routes/projectDeliveryRoutes.js
+++ backend/routes/projectDeliveryRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
     getProjectDeliveries,
 } = require("../controllers/projectDeliveryController");
 
 // Get Project Delivery Dashboard
-router.get("/", getProjectDeliveries);
+router.get("/", authMiddleware,  getProjectDeliveries);
 
 module.exports = router;
