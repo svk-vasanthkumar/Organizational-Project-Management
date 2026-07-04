ec02ae1cfa58ba28b14d69e956a56db98a565eb9
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-04 14:14:20 +0530

    refactor(routes): secure performance routes

--- backend/routes/performanceRoutes.js
+++ backend/routes/performanceRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
     getPerformance
 } = require("../controllers/performanceController");
 
 // Get performance analytics metrics aggregated for all team members
-router.get("/", getPerformance);
+router.get("/", authMiddleware, getPerformance);
 
 module.exports = router;
