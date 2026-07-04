0066cf1d7244ff4351abab6d840c9f1c4125ac6f
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-04 14:16:13 +0530

    Configure axios to send JWT token

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,37 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "http://localhost:5000/api"
+const api = axios.create({
+    baseURL: "http://localhost:5000/api",
 });
+
+// Automatically attach JWT token to every request
+api.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+
+    },
+    (error) => Promise.reject(error)
+);
+
+// Handle unauthorized responses
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+
+        if (error.response?.status === 401) {
+            localStorage.clear();
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default api;
